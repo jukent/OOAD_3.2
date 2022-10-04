d272dacc66f3e2a9d4577ac3c1edef6f0fcb47d5
Julia Kent <46687291+jukent@users.noreply.github.com>
2022-10-04 17:26:48 -0600

    Update src/main/java/celebration/JumpCelebration.java

--- src/main/java/celebration/JumpCelebration.java
+++ src/main/java/celebration/JumpCelebration.java
@@ -58,7 +58,7 @@ public class JumpCelebration extends Celebration {
         if (this.celebrationRef != null) {
             results += this.celebrationRef.trackCelebrate();
         }
-        for(int i = 0; i < DiceRolls.rollDice(3) - 1; i++) {
+        for (int i = 0; i < DiceRolls.rollDice(3) - 1; i++) {
             Results += "Jump! ";
         }
         return Results;
