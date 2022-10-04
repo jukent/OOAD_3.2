415e865c51729a16aacc29b5d5298bd14cc0f390
Julia Kent <46687291+jukent@users.noreply.github.com>
2022-10-04 17:27:20 -0600

    Update src/main/java/celebration/ShoutCelebration.java

--- src/main/java/celebration/ShoutCelebration.java
+++ src/main/java/celebration/ShoutCelebration.java
@@ -54,7 +54,7 @@ public class ShoutCelebration extends Celebration {
     }
 
     public String trackCelebrate() {
-        String Results = "";
+        String results = "";
         if(this.celebrationRef != null){Results += this.celebrationRef.trackCelebrate();}
         for(int i = 0; i < DiceRolls.rollDice(3) - 1; i++) {
             Results += "Shout! ";
