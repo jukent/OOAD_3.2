da0034366bcb2f1e27e0a6956c3ae03d9a558dfb
Chaps42 <84743383+Chaps42@users.noreply.github.com>
2022-10-04 12:31:44 -0600

    Update FightBehaviorTest.java
    Added fight behavior tests

--- src/test/java/FightBehaviorTest.java
+++ src/test/java/FightBehaviorTest.java
@@ -1,3 +1,43 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import dungeon.Dungeon;
+import entity.Character;
+import entity.*;
+
+
 public class FightBehaviorTest {
-    
+    Dungeon dungeon = new Dungeon();
+    Character A = new Runner(1, dungeon);
+    Character B = new Brawler(1,dungeon);
+    Character C = new Sneaker(1, dungeon);
+    Character D = new Thief(1,dungeon);
+
+
+
+    @Test
+    public void testRunnerFightType(){
+        Assert.assertEquals("Untrained",A.getFightType());
+
+    }
+
+    @Test
+    public void testBrawlerFightType(){
+        Assert.assertEquals("Expert",B.getFightType());
+
+    }
+
+    @Test
+    public void testSneakerFightType(){
+        Assert.assertEquals("Stealth",C.getFightType());
+
+    }
+
+    @Test
+    public void testThiefFightType(){
+        Assert.assertEquals("Trained",D.getFightType());
+
+    }
+
 }
